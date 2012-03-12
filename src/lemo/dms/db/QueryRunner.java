d3cb6ef12d2e1f1aa42d2346817eaf4481ba041f
Leonard Kappe <kappe@htw-berlin.de>
2012-03-12 15:10:18 +0100

    dbcp (connection pooling)

--- src/lemo/dms/db/QueryRunner.java
+++ src/lemo/dms/db/QueryRunner.java
@@ -1,15 +1,14 @@
 package lemo.dms.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 import lemo.dms.core.ServerConfigurationHardCoded;
 import lemo.dms.processing.Query;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,23 +18,54 @@ import org.apache.log4j.Logger;
  * @author Leonard Kappe
  */
 public class QueryRunner {
+
 	private Logger log = ServerConfigurationHardCoded.getInstance().getLogger();
-	private Connection connection;
+	private BasicDataSource dataSource;
 
-	public void connect() throws SQLException {
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", "username"); 
-		connectionProps.put("password", "password");
-		connection = DriverManager.getConnection("connection",
-				connectionProps); // TODO use a real connection string 
+	public QueryRunner() {
+		dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("jdbc.driver.DBDriver");
+		dataSource.setUsername("user");
+		dataSource.setPassword("password");
+		dataSource.setUrl("uri");
+	}
 
-		log.info("Database connected");
+	public void closeDataSource() throws SQLException {
+		dataSource.close();
 	}
 
 	public ResultSet run(Query query) throws SQLException {
 		log.info("Running query " + query);
-		Statement statement = connection.createStatement();
-		return statement.executeQuery(query.getSqlQuery());
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet result = null;
+
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.createStatement();
+			result = statement.executeQuery(query.getSqlQuery());
+			log.info("Query " + query + " excecuted");
+
+			int j = 0;
+			int numcols = result.getMetaData().getColumnCount();
+			while (result.next()) {
+				for (int i = 1; i <= numcols; i++) {
+					log.debug("Row: " + j + ", Column: " + i + ", Result: "
+							+ result.getString(i));
+				}
+				j++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (result != null)
+				result.close();
+			if (statement != null)
+				statement.close();
+			if (connection != null)
+				connection.close();
+		}
+		return result;
 	}
 
 }
