feb3250f59cbe189d9897e822452e8e89db8c9a8
Leonard Kappe <kappe@htw-berlin.de>
2012-03-12 15:49:55 +0100

    connection pooling implementation

--- src/lemo/dms/db/QueryRunner.java
+++ src/lemo/dms/db/QueryRunner.java
@@ -1,71 +1,82 @@
 package lemo.dms.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.sql.DataSource;
+
 import lemo.dms.core.ServerConfigurationHardCoded;
 import lemo.dms.processing.Query;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.dbcp.PoolingDriver;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import org.apache.log4j.Logger;
 
 /**
  * 
- * 
+ * Manages database connections and handles execution of queries.
  * 
  * @author Leonard Kappe
  */
 public class QueryRunner {
 
-	private Logger log = ServerConfigurationHardCoded.getInstance().getLogger();
-	private BasicDataSource dataSource;
+    private static PoolableConnectionFactory connectionFactory;
+    private Logger log = ServerConfigurationHardCoded.getInstance().getLogger();
 
-	public QueryRunner() {
-		dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("jdbc.driver.DBDriver");
-		dataSource.setUsername("user");
-		dataSource.setPassword("password");
-		dataSource.setUrl("uri");
-	}
+    public static void setupDriver(String connectionURI, String userName, String password) throws Exception {
+        connectionFactory = new PoolableConnectionFactory(new DriverManagerConnectionFactory(connectionURI, userName,
+                password), new GenericObjectPool<Object>(null), null, null, false, true);
 
-	public void closeDataSource() throws SQLException {
-		dataSource.close();
-	}
+        // Create the PoolingDriver
+        Class.forName("org.apache.commons.dbcp.PoolingDriver");
+        PoolingDriver driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+        driver.registerPool("example", connectionFactory.getPool());
+    }
 
-	public ResultSet run(Query query) throws SQLException {
-		log.info("Running query " + query);
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet result = null;
+    public static void close() throws SQLException {
+        PoolingDriver driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+        driver.closePool("example");
+    }
 
-		try {
-			connection = dataSource.getConnection();
-			statement = connection.createStatement();
-			result = statement.executeQuery(query.getSqlQuery());
-			log.info("Query " + query + " excecuted");
+    public ResultSet run(Query query) throws SQLException {
+        log.info("Running query " + query);
 
-			int j = 0;
-			int numcols = result.getMetaData().getColumnCount();
-			while (result.next()) {
-				for (int i = 1; i <= numcols; i++) {
-					log.debug("Row: " + j + ", Column: " + i + ", Result: "
-							+ result.getString(i));
-				}
-				j++;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (result != null)
-				result.close();
-			if (statement != null)
-				statement.close();
-			if (connection != null)
-				connection.close();
-		}
-		return result;
-	}
+        DataSource dataSource = new PoolingDataSource(connectionFactory.getPool());
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet result = null;
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            result = statement.executeQuery(query.getSqlQuery());
+            log.info("Query " + query + " excecuted");
+
+            int j = 0;
+            int numcols = result.getMetaData().getColumnCount();
+            while (result.next()) {
+                for (int i = 1; i <= numcols; i++) {
+                    log.debug("Row: " + j + ", Column: " + i + ", Result: " + result.getString(i));
+                }
+                j++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (result != null)
+                result.close();
+            if (statement != null)
+                statement.close();
+            if (connection != null)
+                connection.close();
+        }
+        return result;
+    }
 
 }
