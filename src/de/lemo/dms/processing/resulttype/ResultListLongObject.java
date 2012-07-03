74ddf7046c2b1a636d1dbc85718032b0e9c43926
Leonard Kappe <kappe@htw-berlin.de>
2012-07-03 16:02:16 +0200

    Dummy type variable, avoid null in empty lists

--- src/de/lemo/dms/processing/resulttype/ResultListLongObject.java
+++ src/de/lemo/dms/processing/resulttype/ResultListLongObject.java
@@ -5,27 +5,28 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
 @XmlRootElement
 public class ResultListLongObject {
 
-	
-	private List<Long> elements;
-	
-	public ResultListLongObject()
-	{
-		
-	}
-	
-	public ResultListLongObject(List<Long> elements)
-	{
-		this.elements = elements;
-	}
-	
-	@XmlElement
-	public List<Long> getElements()
-	{
-		return this.elements;
-	}
-	
+    @XmlElement
+    private String type = getClass().getSimpleName();
+
+    private List<Long> elements;
+
+    public ResultListLongObject()
+    {
+
+    }
+
+    public ResultListLongObject(List<Long> elements)
+    {
+        this.elements = elements;
+    }
+
+    @XmlElement
+    public List<Long> getElements()
+    {
+        return this.elements;
+    }
+
 }
