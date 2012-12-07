6e70d734561a6ffe86f0337101eefbbe06e8cb6c
Sebastian Schwarzrock <sschwarzrock@beuth-hochschule.de>
2012-12-07 09:40:27 +0100

    - prepared branch for merge

--- src/main/java/de/lemo/dms/core/DMSMain.java
+++ src/main/java/de/lemo/dms/core/DMSMain.java
@@ -19,7 +19,6 @@ public class DMSMain {
 
         try {
             DMSRemoteServer.INSTANCE.start();
-            Test.run();
             System.in.read(); // TODO every example uses this to keep the server running but there may be a better way
             
         } catch (IOException e) {
