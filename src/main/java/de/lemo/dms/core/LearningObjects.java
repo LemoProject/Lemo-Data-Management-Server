e8caa2e959806c30ec2f41dbe6f44efef93b641b
Andreas Pursian <andreas.pursian@hwr-berlin.de>
2012-12-10 15:12:14 +0100

    - uncommented "chat" type in learningobjects enum class

--- src/main/java/de/lemo/dms/core/LearningObjects.java
+++ src/main/java/de/lemo/dms/core/LearningObjects.java
@@ -3,7 +3,7 @@ package de.lemo.dms.core;
 public enum LearningObjects {
 	resource,
 	assignment,
-	//chat,  //kein chat, da keine verbindung zu kursen
+	chat,  //kein chat, da keine verbindung zu kursen
 	course,
 	forum,
 	question,
