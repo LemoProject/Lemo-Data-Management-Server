54654b46286de4c0e9bcc8aa0bb8c0a4095bd23c
Boris Wenzlaff <boris.wenzlaff@htw-berlin.de>
2012-12-14 11:28:34 +0100

    add test class for Version class

---
+++ src/test/java/de/lemo/dms/core/VersionTest.java
@@ -0,0 +1,24 @@
+package de.lemo.dms.core;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import de.lemo.dms.core.Version;
+
+/**
+ * test for Version class
+ * @author Boris Wenzlaff
+ *
+ */
+public class VersionTest {
+	
+	/**
+	 * check if the version != "unknown"
+	 */
+    @Test
+    public void testDMSVersion() {
+    	Version version = new Version();
+    	String v = version.getServerVersion();
+    	System.out.println("Version: " + v);
+    	assertFalse("wrong version read", v == "unknown");
+    }
+}
