3323ce70dae046f464423859815e346b5b4551d1
Boris Wenzlaff <boris.wenzlaff@htw-berlin.de>
2013-02-25 17:52:52 +0100

    add comment

--- src/main/java/de/lemo/dms/connectors/ConnectorManager.java
+++ src/main/java/de/lemo/dms/connectors/ConnectorManager.java
@@ -17,6 +17,11 @@ import de.lemo.dms.core.config.ServerConfiguration;
 import de.lemo.dms.db.IDBHandler;
 import de.lemo.dms.db.miningDBclass.PlatformMining;
 
+/**
+ * Manager to controle the connectors
+ * @author Boris Wenzlaff
+ *
+ */
 public enum ConnectorManager {
 
 	INSTANCE;
