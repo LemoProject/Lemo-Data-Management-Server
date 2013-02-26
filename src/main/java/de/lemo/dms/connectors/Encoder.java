d995a59fc035ab51cddb70d12740b2c7c7b94edf
Leonard Kappe <kappe@htw-berlin.de>
2013-02-26 14:04:33 +0100

    Move encoder initialization

--- src/main/java/de/lemo/dms/connectors/Encoder.java
+++ src/main/java/de/lemo/dms/connectors/Encoder.java
@@ -7,11 +7,13 @@
 package de.lemo.dms.connectors;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * MD5 operation for anonymity
  * 
  * @author Sebastian Schwarzrock
+ * @author Leonard Kappe
  */
 public final class Encoder {
 
@@ -19,7 +21,14 @@ public final class Encoder {
 
 	}
 
-	private static MessageDigest md = null;
+	private static MessageDigest md;
+	static {
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// md5 is always available, it's a java platform requirement
+		}
+	}
 
 	/**
 	 * Returns the MD5 value of the input-string.
@@ -28,23 +37,12 @@ public final class Encoder {
 	 *            String to encode
 	 * @return MD5-encoded string
 	 */
-	public static String createMD5(String in)
-	{
-		try {
-			Encoder.md = MessageDigest.getInstance("MD5");
-			Encoder.md.update(in.getBytes());
-
-			final byte byteData[] = Encoder.md.digest();
-
-			final StringBuffer sb = new StringBuffer();
-
-			for (int i = 0; i < byteData.length; i++) {
-				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-			}
-			in = sb.substring(0);
-		} catch (final Exception e)
-		{
+	public static String createMD5(String in) {
+		final byte byteData[] = Encoder.md.digest(in.getBytes());
+		final StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toHexString((byteData[i] & 0xff) + 0x100).substring(1));
 		}
-		return in;
+		return sb.toString();
 	}
 }
