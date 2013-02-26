60aa6df6137b515eeb59204cdad8074ceb926bdc
Boris Wenzlaff <boris.wenzlaff@htw-berlin.de>
2013-02-26 10:51:57 +0100

    remove strin equality

--- src/main/java/de/lemo/dms/connectors/moodleNumericId/ExtractAndMapMoodle.java
+++ src/main/java/de/lemo/dms/connectors/moodleNumericId/ExtractAndMapMoodle.java
@@ -1105,7 +1105,7 @@ public class ExtractAndMapMoodle extends ExtractAndMap {// Versionsnummer in Nam
 			else
 			{
 				final ArrayList<Long> times = users.get(uid1);
-				if (loadedItem.getAction() == "login") {
+				if (loadedItem.getAction().equals("login")) {
 					times.add(0L);
 				}
 				if (!times.contains(loadedItem.getTime())) {
@@ -1172,7 +1172,7 @@ public class ExtractAndMapMoodle extends ExtractAndMap {// Versionsnummer in Nam
 			else
 			{
 				final ArrayList<Long> times = users.get(uid);
-				if (loadedItem.getAction() == "login") {
+				if (loadedItem.getAction().equals("login")) {
 					times.add(0L);
 				}
 				if (!times.contains(loadedItem.getTime())) {
