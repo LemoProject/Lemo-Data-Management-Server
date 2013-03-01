a4450bb4fc7bd83e9cacc0bede84502f0537886b
Sebastian Schwarzrock <sschwarzrock@beuth-hochschule.de>
2013-03-01 10:56:51 +0100

    - suck it git!

--- src/main/java/de/lemo/dms/processing/questions/QPerformanceUserTestBoxPlot.java
+++ src/main/java/de/lemo/dms/processing/questions/QPerformanceUserTestBoxPlot.java
@@ -26,7 +26,6 @@ import org.hibernate.criterion.Restrictions;
 
 import de.lemo.dms.core.config.ServerConfiguration;
 import de.lemo.dms.db.IDBHandler;
-import de.lemo.dms.db.miningDBclass.abstractions.ICourseRatedObjectAssociation;
 import de.lemo.dms.db.miningDBclass.abstractions.IRatedLogObject;
 import de.lemo.dms.processing.MetaParam;
 import de.lemo.dms.processing.StudentHelper;
