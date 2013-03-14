76bfb827cc9ad85c2656674a7666709fc9ef8605
Sebastian Schwarzrock <sschwarzrock@beuth-hochschule.de>
2013-03-14 15:41:45 +0100

    - added Class-header

--- src/main/java/de/lemo/dms/db/miningDBclass/abstractions/ICourseLORelation.java
+++ src/main/java/de/lemo/dms/db/miningDBclass/abstractions/ICourseLORelation.java
@@ -1,3 +1,10 @@
+/**
+ * File ./main/java/de/lemo/dms/db/miningDBclass/abstractions/ICourseLORelation.java
+ * Date 2013-03-14
+ * Project Lemo Learning Analytics
+ */
+
+
 package de.lemo.dms.db.miningDBclass.abstractions;
 
 /**
