b3fd8a1a8e7f0071d1dfaa07b9e7baa7c9f8084d
one200 <one200@gmx.de>
2013-07-02 12:26:37 +0200

    Add GPL version 3 licence to all *.java files

--- src/main/java/de/lemo/dms/service/ServiceTaskManager.java
+++ src/main/java/de/lemo/dms/service/ServiceTaskManager.java
@@ -1,3 +1,23 @@
+/**
+ * File ./src/main/java/de/lemo/dms/service/ServiceTaskManager.java
+ * Lemo-Data-Management-Server for learning analytics.
+ * Copyright (C) 2013
+ * Leonard Kappe, Andreas Pursian, Sebastian Schwarzrock, Boris Wenzlaff
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package de.lemo.dms.service;
 
 import java.io.UnsupportedEncodingException;
