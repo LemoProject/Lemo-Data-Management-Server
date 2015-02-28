37f67bbb50df77247944e378bc567f4fe887b7ce
Kluesener <mkluesener@gmx.net>
2015-02-28 09:04:51 +0100

    Added new features.

--- src/main/java/de/lemo/dms/processing/FeatureProcessor.java
+++ src/main/java/de/lemo/dms/processing/FeatureProcessor.java
@@ -19,6 +19,7 @@ import de.lemo.dms.db.mapping.UserAssessment;
 import de.lemo.dms.processing.features.ContentImageCount;
 import de.lemo.dms.processing.features.ContentLinkCount;
 import de.lemo.dms.processing.features.ContentWordCount;
+import de.lemo.dms.processing.features.PostRating;
 import de.lemo.dms.processing.resulttype.UserInstance;
 
 import org.hibernate.Criteria;
@@ -29,129 +30,13 @@ import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
 
+/* Processes and persists Features.
+ * 
+ */
+
 public class FeatureProcessor{
 
-	public void processAll(){
-		List<Collection<?>> persistCollections = new ArrayList<Collection<?>>();
-		List<UserAssessment> courseUser = createAssessmentsFromLearningAttributes();
-		persistCollections.add(courseUser);
-		persistFeatures(persistCollections);
-		System.out.println("End processing!");
-	}
-	
-	//Processes features from learning objects
-	public void processFeatures(){
-		List<Collection<?>> persistCollections = new ArrayList<Collection<?>>();
-		List<LearningAttribute> contentImageCount = new ContentImageCount().getLearningAttributes();
-		persistCollections.add(contentImageCount);
-		persistFeatures(persistCollections);
-		persistCollections = new ArrayList<Collection<?>>();
-		List<LearningAttribute> contentLinkCount = new ContentLinkCount().getLearningAttributes();
-		persistCollections.add(contentLinkCount);
-		persistFeatures(persistCollections);
-		persistCollections = new ArrayList<Collection<?>>();
-		List<LearningAttribute> contentWordCount = new ContentWordCount().getLearningAttributes();
-		persistCollections.add(contentWordCount);
-		persistFeatures(persistCollections);
-		System.out.println("End feature processing!");		
-	}
-
-	public List<UserAssessment> createAssessmentsFromLearningAttributes(){
-		List<UserAssessment> userAssessments = new ArrayList<UserAssessment>();
-		Session session = ServerConfiguration.getInstance().getMiningDbHandler().getMiningSession();
-		long nextId = getNextUnusedIdInEntity(UserAssessment.class);
-				
-		Criteria criteria = session.createCriteria(Course.class);
-		List<Course> courses = criteria.list();
-		for(Course course : courses){
-			criteria = session.createCriteria(CourseUser.class,"courseUser");
-			criteria.add(Restrictions.eq("courseUser.course", course));
-			List<CourseUser> courseUsers = criteria.list();
-			for(CourseUser courseUser : courseUsers){
-				List<String> contentAttributeNames = new ArrayList<String>();
-				Map<String,UserAssessment> assessments = new HashMap<String, UserAssessment>();				
-				contentAttributeNames.add("Content_Linkcount");
-				contentAttributeNames.add("Content_Imagecount");
-				contentAttributeNames.add("Post_Up_Votes");
-				contentAttributeNames.add("Post_Down_Votes");
-				contentAttributeNames.add("Content_Wordcount");
-				for(String contentAttributeName : contentAttributeNames){
-					UserAssessment userAssessment = new UserAssessment();
-					userAssessment.setCourse(course);
-					userAssessment.setUser(courseUser.getUser());
-					userAssessment.setId(nextId);
-					userAssessment.setFeedback(contentAttributeName);
-					userAssessment.setGrade(0D);
-					assessments.put(contentAttributeName, userAssessment);
-					nextId++;					
-				}				
-				criteria = session.createCriteria(CollaborationLog.class, "collaborationLog");
-				criteria.add(Restrictions.eq("collaborationLog.course", course));
-				criteria.add(Restrictions.eq("collaborationLog.user", courseUser.getUser()));
-				List<CollaborationLog> collaborationLogs = criteria.list();
-				if(!collaborationLogs.isEmpty()){
-					for(CollaborationLog collaborationLog : collaborationLogs){
-						criteria = session.createCriteria(LearningAttribute.class, "learningAttribute");
-						criteria.add(Restrictions.eq("learningAttribute.learning", collaborationLog.getLearning()));
-						List<LearningAttribute> learningAttributes = criteria.list();
-						for(LearningAttribute learningAttribute : learningAttributes){
-							if(learningAttribute.getAttribute().getName().equals("Content_Linkcount")){								
-								assessments.get("Content_Linkcount")
-									.setGrade(assessments.get("Content_Linkcount").getGrade()+
-												Integer.valueOf(learningAttribute.getValue()));
-							}	
-							else if(learningAttribute.getAttribute().getName().equals("Post_Up_Votes")){								
-								assessments.get("Post_Up_Votes")
-									.setGrade(assessments.get("Post_Up_Votes").getGrade()+
-												Integer.valueOf(learningAttribute.getValue()));
-							}
-							else if(learningAttribute.getAttribute().getName().equals("Post_Down_Votes")){								
-								assessments.get("Post_Down_Votes")
-									.setGrade(assessments.get("Post_Down_Votes").getGrade()+
-												Integer.valueOf(learningAttribute.getValue()));
-							}
-							else if(learningAttribute.getAttribute().getName().equals("Content_Wordcount")){								
-								assessments.get("Content_Wordcount")
-									.setGrade(assessments.get("Content_Wordcount").getGrade()+
-												Integer.valueOf(learningAttribute.getValue()));
-							}
-							else if(learningAttribute.getAttribute().getName().equals("Content_Imagecount")){								
-								assessments.get("Content_Imagecount")
-									.setGrade(assessments.get("Content_Imagecount").getGrade()+
-												Integer.valueOf(learningAttribute.getValue()));
-							}
-						}
-					}
-				}
-				userAssessments.addAll(assessments.values());
-			}
-			System.out.println(course.getTitle()+" #UsersAssessments cumulative: "+userAssessments.size());
-		}
-		session.close();
-		return userAssessments;
-	}
-
-	private long getNextUnusedIdInEntity(Class<?> entityClass) {
-		long nextId=0;
-		Session session = ServerConfiguration.getInstance().getMiningDbHandler().getMiningSession();
-		Criteria criteria = session.createCriteria(entityClass);
-		criteria.addOrder(Order.desc("id"));
-		criteria.setMaxResults(1);
-		UserAssessment userAssessmentMaxId = (UserAssessment) criteria.uniqueResult();
-		if(userAssessmentMaxId!=null){
-			nextId = userAssessmentMaxId.getId()+1;
-		}
-		session.close();
-		return nextId;
-	}
-
-	private void persistFeatures(List<Collection<?>> persistCollections) {
-		IDBHandler dbHandler = ServerConfiguration.getInstance().getMiningDbHandler();	
-		Session session = ServerConfiguration.getInstance().getMiningDbHandler().getMiningSession();		
-		dbHandler.saveCollectionToDB(session, persistCollections);
-		session.close();
-	}
-
+	//Generates all features for all users in the given course.
 	public List<UserInstance> generateFeaturesForCourseUsers(Long courseId) {
 		List<UserInstance> userInstances = new ArrayList<UserInstance>();
 		Session session = ServerConfiguration.getInstance().getMiningDbHandler().getMiningSession();
@@ -162,11 +47,7 @@ public class FeatureProcessor{
 		for(User user : users){
 			UserInstance userInstance = new UserInstance();
 			userInstance.setUserId(user.getId());
-			criteria = session.createCriteria(CollaborationLog.class);
-			criteria.add(Restrictions.eq("course.id", courseId));
-			criteria.add(Restrictions.eq("user", user));
-			List<CollaborationLog> logsFromUser = criteria.list();
-			insertAllFeaturesFromLog(userInstance,logsFromUser,session);
+			insertAllFeaturesFromLog(userInstance,user,courseId,session);
 			insertUserAssessment(userInstance,user,courseId,session);
 			userInstances.add(userInstance);
 		}
@@ -206,8 +87,11 @@ public class FeatureProcessor{
 	}
 
 	private void insertAllFeaturesFromLog(UserInstance userInstance,
-			List<CollaborationLog> logsFromUser,Session session) {
-		Criteria criteria;
+			User user, Long courseId,Session session) {
+		Criteria criteria = session.createCriteria(CollaborationLog.class);
+		criteria.add(Restrictions.eq("course.id", courseId));
+		criteria.add(Restrictions.eq("user", user));
+		List<CollaborationLog> logsFromUser = criteria.list();
 		userInstance.setForumUsed(true);
 		for(CollaborationLog logFromUser: logsFromUser){
 			if(logFromUser.getAction().equals("vote") && logFromUser.getText().equals("up")){
@@ -246,6 +130,18 @@ public class FeatureProcessor{
 			}
 			else if(learningAttribute.getAttribute().getName().equals("Content_Imagecount")){								
 				userInstance.setImageCount(userInstance.getImageCount()+Integer.valueOf(learningAttribute.getValue()));
+			}
+			else if(learningAttribute.getAttribute().getName().equals("PostRating")){	
+				int max = userInstance.getPostRatingMax();
+				int min = userInstance.getPostRatingMin();
+				int current = Integer.valueOf(learningAttribute.getValue());
+				userInstance.setPostRatingSum(userInstance.getPostRatingSum()+current);
+				if(current > max){
+					userInstance.setPostRatingMax(current);
+				}
+				if(current < min){
+					userInstance.setPostRatingMin(current);
+				}
 			}
 		}		
 	}
