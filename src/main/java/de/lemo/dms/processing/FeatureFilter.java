01d42657e17b7125ce5dd341a55ea31953fc1f80
Kluesener <mkluesener@gmx.net>
2015-03-03 09:36:16 +0100

    Added filter to calculate class values.

--- src/main/java/de/lemo/dms/processing/FeatureFilter.java
+++ src/main/java/de/lemo/dms/processing/FeatureFilter.java
@@ -17,4 +17,26 @@ public class FeatureFilter {
 		}
 		return filteredUserInstances;		
 	}
+	
+	//Calculates the class using 80% of the maximum completed segments.
+	//A problem could be users making old assessments.
+	public List<UserInstance> calculateClassValue(List<UserInstance> userInstances){
+		int max = 0;
+		for(UserInstance userInstance : userInstances){
+			if(userInstance.getSegmentProgress()>max){
+				max = userInstance.getSegmentProgress();
+			}
+		}
+		double passingThreshold = (double)max/100*80;
+		System.out.println("Passing Threshold: " + passingThreshold);
+		for(UserInstance userInstance : userInstances){
+			if(userInstance.getSegmentProgress()>passingThreshold){
+				userInstance.setClassId(1);
+				//System.out.println("Segments: "+ userInstance.getSegmentProgress());
+			} else {
+				userInstance.setClassId(0);
+			}
+		}		
+		return userInstances;	
+	}
 }
